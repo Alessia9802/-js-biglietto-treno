196949f3e7151696197f6ab11f62d5163b48fc9d
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-11 15:15:09 +0200

    Domande km viaggio e età passeggero

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//chiedere all'utente il numero di chilometri che vuole percorrere
+
+
+const km_viaggio = prompt("Quanti km vuoi percorrere?");
+console.log(km_viaggio);
+
+// chiedere l'età del passeggero.
+
+const passenger_age = prompt("Quanti anni hai?");
+console.log(passenger_age);
+
+/*Calcolare il prezzo totale del viaggio:
+ - Prezzo del biglietto vale 0.21€ al km 
+ */
+
+// Applicare sconto del 20% ai minorenni
+
+
+// Applicare sconto del 40% agli over 65 anni
+
+// Prezzo finale
+
+if () {
+
+ }
+
+const prezzo_finale = 
